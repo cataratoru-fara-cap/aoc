25f18f644e775b48676f932f210e934398088092
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-12-06 17:18:00 +0200

    Finished day5 part one

--- solution_5.py
+++ solution_5.py
@@ -1,9 +1,30 @@
-file = open("demo5.txt", "r").read().split("\n\n")
+file = open("input5.txt", "r").read().split("\n\n")
 processed_data_dict = {}
 for i in range(len(file)):
     key, codes = file[i].strip().split(":") 
     codes = [item.split() for item in codes.split("\n") if item != ""]
     processed_data_dict[key] = codes
 
-for k,v in processed_data_dict.items():
+for k, v in processed_data_dict.items():
     print(k, v)
+
+
+def locator(seed):
+    curNum = seed
+    for values in list(processed_data_dict.values())[1:]:  # excluedes seeds from parsing
+        for lst in values:
+            d_start, src_start, r = int(lst[0]), int(lst[1]), int(lst[2])
+            if src_start <= curNum < src_start + r:
+                curNum = d_start + (curNum - src_start)
+                break
+    return curNum
+
+
+locations = []
+for loc in list(processed_data_dict.values())[0]:  # gets the seed values
+    for sub_loc in loc:  # loc is a list of lists with values
+        seed_dest = locator(int(sub_loc))
+        locations.append(seed_dest)
+
+print(locations)
+print(min(locations))
