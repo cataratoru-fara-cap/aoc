9e7d8f55777daab4d7c043c1a705579b9d0191db
cataratoru-fara-cap <gabivaireanu@gmail.com>
2023-12-08 18:23:34 +0200

    Deleted brute force attempt at p2 and refactored some assignements

--- solution_5.py
+++ solution_5.py
@@ -1,9 +1,12 @@
-file = open("input5.txt", "r").read().split("\n\n")
+file = open("demo5.txt", "r").read().split("\n\n")
 processed_data_dict = {}
 for i in range(len(file)):
     key, codes = file[i].strip().split(":")
-    codes = [item.split() for item in codes.split("\n") if item != ""]
+    codes = [tuple(int(i) for i in item.split()) for item in codes.split("\n") if item != ""]  # noqa
     processed_data_dict[key] = codes
+
+raw_seeds = [i for i in list(processed_data_dict.values())[0][0]]
+processed_data_dict["seeds"] = raw_seeds
 
 # for k, v in processed_data_dict.items():
 #     print(k, v)
@@ -13,30 +16,27 @@ def locator(seed):
     curNum = seed
     for values in list(processed_data_dict.values())[1:]:  # noqa excluedes seeds from parsing
         for lst in values:
-            d_start, src_start, r = int(lst[0]), int(lst[1]), int(lst[2])
+            d_start, src_start, r = lst[0], lst[1], lst[2]
             if src_start <= curNum < src_start + r:
                 curNum = d_start + (curNum - src_start)
                 break
     return curNum
 
 
-seeds = [int(i) for i in list(processed_data_dict.values())[0][0]]
-
 locations_1 = []
-for loc in seeds:  # gets the seed values
+for loc in raw_seeds:  # gets the seed values
     seed_dest = locator(int(loc))
     locations_1.append(seed_dest)
 
 # print(locations_1)
 # print(min(locations_1))
 
-locations_2 = []
+processed_data_dict["seeds"] = [(raw_seeds[i], raw_seeds[i+1])
+                                for i in range(0, len(raw_seeds), 2)
+                                ]
 
-for i in range(0, len(seeds) - 1, 2):
-    for k in range(seeds[i], seeds[i] + seeds[i+1]):  # seeds[i] + seeds[i+1]):
-        seed_dest = locator(k)
-        print(seed_dest)
-        locations_2.append(seed_dest)
+seeds = processed_data_dict["seeds"]
+maps = list(processed_data_dict.values())[1:]
 
-# print(locations_2)
-print(min(locations_2))
+print(seeds, "\n", maps)
+
