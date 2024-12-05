f0facc059f6a6ab6e07cbfa5c471aae0f7d4af18
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-12-05 21:27:33 +0200

    Day 4 Solution

--- aoc 2024/4/4.py
+++ aoc 2024/4/4.py
@@ -1,45 +1,101 @@
-import numpy as np
+# part1
+def star_search(chr: str, pos: tuple, size: tuple, mat: list) -> int:
+    sum = 0
+    y = pos[0]
+    x = pos[1]
+    if chr == 'X':
+        if line(x, y, "left", size) and (mat[y][x-1] + mat[y][x-2] + mat[y][x-3] == "MAS"):
+            sum += 1
 
-file = open("demo.txt","r")
+        if line(x, y, "up", size) and (mat[y-1][x] + mat[y-2][x] + mat[y-3][x] == "MAS"):
+            sum += 1
+
+        if line(x, y, "right", size) and (mat[y][x+1] + mat[y][x+2] + mat[y][x+3] == "MAS"):
+            sum += 1
+
+        if line(x, y, "down", size) and (mat[y+1][x] + mat[y+2][x] + mat[y+3][x] == "MAS"):
+            sum += 1
+
+        if line(x, y, "dRightUp", size) and mat[y-1][x+1] + mat[y-2][x+2] + mat[y-3][x+3] == "MAS":
+            sum += 1 
+
+        if line(x, y, "dLeftUp", size) and mat[y-1][x-1] + mat[y-2][x-2] + mat[y-3][x-3] == "MAS":
+            sum += 1 
+
+        if line(x, y, "dRightDown", size) and mat[y+1][x+1] + mat[y+2][x+2] + mat[y+3][x+3] == "MAS":
+            sum += 1 
+
+        if line(x, y, "dLeftDown", size) and mat[y+1][x-1] + mat[y+2][x-2] + mat[y+3][x-3] == "MAS":
+            sum += 1
+
+    return sum
+
+
+def line(x: int, y: int, orientation: str, size: tuple) -> bool:
+    match orientation:
+        case "left":
+            return (x - 3 >= 0)
+        case "up":
+            return (y - 3 >= 0)
+        case "right":
+            return (x + 3 < size[1])
+        case "down":
+            return (y + 3 < size[0])
+        case "dRightUp":
+            return (x + 3 < size[1]) and (y - 3 >= 0)
+        case "dLeftUp":
+            return (x - 3 >= 0) and (y - 3 >= 0)
+        case "dRightDown":
+            return (x + 3 < size[1]) and (y + 3 < size[0])
+        case "dLeftDown":
+            return (x - 3 >= 0) and (y + 3 < size[0])
+        case _:
+            raise ValueError(
+                "Invalid Orientation (righht/left/up/down/dRightUp/"
+                "dLeftUp/dRightDown/dLeftDown)"
+            )
+
+
+# file = open("input.txt", "r")
+# file = file.read().splitlines()
+# file = [list(string) for string in file]
+
+# part 1
+# sum = 0
+# for i in range(len(file)):
+#     for j in range(len(file[i])):
+#         sum += star_search(file[i][j], (i, j), (len(file), len(file[0])), file)
+# print(sum)
+
+# part 2
+
+
+def x_search(chr: str, pos: tuple, size: tuple, mat: list) -> int:
+    def diag() -> bool:
+        if (mat[y-1][x+1] == "M" and mat[y+1][x-1] == "S") or \
+           (mat[y+1][x-1] == "M" and mat[y-1][x+1] == "S"):
+            return True
+        return False
+
+    def anti_diag() -> bool:
+        if (mat[y-1][x-1] == "M" and mat[y+1][x+1] == "S") or \
+           (mat[y+1][x+1] == "M" and mat[y-1][x-1] == "S"):
+            return True
+        return False
+
+    y = pos[0]
+    x = pos[1]
+    if (mat[y][x] == "A" and diag() and anti_diag()):
+        return 1
+    return 0
+
+
+file = open("input.txt", "r")
 file = file.read().splitlines()
+file = [list(string) for string in file]
 
-def compare(my_list: str) -> int:
-    res = 0
-    for i in range(len(my_list) - 3):
-        if my_list[i:i+4] == "XMAS":
-            res += 1
-    return res
-
-def get_diagonals(matrix):
-    diagonals = []
-    np_matrix = np.array(matrix)
-    rows, cols = np_matrix.shape
-
-    for offset in range(-rows + 1, cols):
-        diagonal = np.diagonal(np_matrix, offset=offset)
-        diagonals.append(diagonal.tolist())
-    
-    return diagonals
-
-
-rows = file
-rows_reverse = [''.join(reversed(string)) for string in file]
-
-columns = [[char for char in line] for line in file]
-columns = np.array(columns).T
-columns = [''.join(list )for list in columns]
-columns_reverse = [''.join(reversed(string)) for string in columns]
-
-diagonals = get_diagonals([[char for char in line] for line in file])
-diagonals = [''.join(line) for line in diagonals]
-diagonals_reverse = [''.join(reversed(string)) for string in diagonals]
-
-# print("Rows:", rows)
-# print("Reversed Rows:", rows_reverse)
-# print("Columns:", columns)
-# print("Reversed Columns:", columns_reverse)
-# print("Diagonals:", diagonals)
-# print("Reversed Diagonals:", diagonals_reverse)
-to_compare = [rows, rows_reverse, columns, columns_reverse, diagonals, diagonals_reverse]
-sum = sum(sum([compare(line) for line in text]) for text in to_compare)
-print(sum)
+sum2 = 0
+for i in range(1, len(file)-1):
+    for j in range(1, len(file[i])-1):
+        sum2 += x_search(file[i][j], (i, j), (len(file), len(file[i])), file)
+print(sum2)
